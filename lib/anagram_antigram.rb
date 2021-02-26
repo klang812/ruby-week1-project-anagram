8bd383bbb8c74c9da441bb9529ebd59f1c36d4f0
Kerry Lang <klang812@gmail.com>
2021-02-26 09:02:17 -0800

    add notes to project

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,8 +1,12 @@
 class Anagram
 
-def anagram_antigram(word_one)
-  word_one
+
+def anagram_antigram(word, word_one)
+  words = word.split('').sort
+  word_sort = words.include?(word_one)
   
   end
   
 end
+
+words = ["ruby", "bury"]
