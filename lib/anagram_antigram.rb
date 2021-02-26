9d31fcd8b7d095aad3f79b8853124fff45eb78ba
Kerry Lang <klang812@gmail.com>
2021-02-26 08:20:46 -0800

    set up env and first test  good fail

---
+++ lib/anagram_antigram.rb
@@ -0,0 +1,8 @@
+class Anagram
+
+def anagram_antigram(word_one)
+  word_one
+  
+  end
+  
+end
---
+++ spec/anagram_antigram_spec.rb
@@ -0,0 +1,12 @@
+require('rspec')
+require('anagram_antigram')
+require('pry')
+
+describe('#anagram_antigram') do
+  it("checks to to see if 2 words are anagrams") do
+    anagram = Anagram.new()
+    expect(anagram.anagram_antigram("ruby")).to(eq("bury"))
+  end
+
+  
+end
