10c8099ad4b610ffd155a60c025d0c3a457eca36
Kerry Lang <klang812@gmail.com>
2021-02-26 15:07:35 -0800

    add good fail multi word string

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,22 +1,22 @@
 class Anagram
 
-
-
   def anagram_antigram(phrase)
     word_list = phrase.split(' ')
     new_word_list = word_list.collect{|x| x.downcase.split('').sort.join}
     for word in new_word_list do
       if new_word_list.count(word) > 1
-        puts true
+        true
       else
-        puts false
+        false
       end
       
     end 
+    
   end
 end
 
 words = ["ruby", "bury"]
+
 
 
 
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -5,11 +5,16 @@ require('pry')
 describe('#anagram_antigram') do
   it("checks to see if 2 words are anagrams") do
     anagram = Anagram.new()
-    expect(anagram.anagram_antigram("ruby" "bury")).to(eq(true))
+    expect(anagram.anagram_antigram("ruby bury")).to(eq(true))
   end
 
   it("puts words into lower case to match") do
     anagram = Anagram.new()
-    expect(anagram.anagram_antigram("Ruby" "Bury")).to(eq(true))
+    expect(anagram.anagram_antigram("Ruby Bury")).to(eq(true))
+  end
+
+  it("takes multi word string and finds anagram") do
+    anagram = Anagram.new()
+    expect(anagram.anagram_antigram("Ruby bury")).to(eq(true))
   end
 end
