95b1297ea4667e4061f1639919d36ea910b70a73
Kerry Lang <klang812@gmail.com>
2021-02-26 12:00:30 -0800

    add test 2 good fail

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -6,8 +6,8 @@ class Anagram
   # end
 
   def anagram_antigram(word1, word2)
-    word_one = word1.split('').sort.join
-    word_two = word2.split('').sort.join
+    word_one = word1.split('').sort.join.downcase
+    word_two = word2.split('').sort.join.downcase
     if word_one == word_two 
       true
     else
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -3,10 +3,13 @@ require('anagram_antigram')
 require('pry')
 
 describe('#anagram_antigram') do
-  it("checks to to see if 2 words are anagrams") do
+  it("checks to see if 2 words are anagrams") do
     anagram = Anagram.new()
     expect(anagram.anagram_antigram("ruby", "bury")).to(eq(true))
   end
 
-  
+  it("puts words into lower case to match") do
+    anagram = Anagram.new()
+    expect(anagram.anagram_antigram("Ruby", "Bury")).to(eq(true))
+  end
 end
