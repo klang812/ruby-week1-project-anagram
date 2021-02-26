887ad2455fc30badf8d044efac779ce025618507
Kerry Lang <klang812@gmail.com>
2021-02-26 15:22:07 -0800

    add test pass for multi word string

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -4,14 +4,12 @@ class Anagram
     word_list = phrase.split(' ')
     new_word_list = word_list.collect{|x| x.downcase.split('').sort.join}
     for word in new_word_list do
-      if new_word_list.count(word) > 1
-        true
-      else
-        false
+      if new_word_list.count(word) > 1 
+        return true
       end
       
     end 
-    
+    return false
   end
 end
 
