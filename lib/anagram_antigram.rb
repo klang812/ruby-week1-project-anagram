3cc08094bcbcfb3354c5e973cb89ee3df383fdc1
Kerry Lang <klang812@gmail.com>
2021-02-26 11:24:04 -0800

    research anagram logic stuck

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,13 +1,27 @@
 class Anagram
 
-def anagram_antigram(word)
-  word = word.split('').sort
-  #new_word = word.include?.join
-  # if word.match?(words) do
-  #   true
-  
+  # def initialize
+  #   @word_one = word_one
+  #   @word_two = word_two
   # end
+
+  def anagram_antigram(word1, word2)
+    word_one = word1.split('').sort
+    word_two = word2.split('').sort
+    word_one.include?(word_two) 
+      
+    
+  
 end
 end
 
 words = ["ruby", "bury"]
+
+
+
+
+#new_word = word.include?.join
+    # if word.match?(words) do
+    #   true
+    
+    # end 
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -5,7 +5,7 @@ require('pry')
 describe('#anagram_antigram') do
   it("checks to to see if 2 words are anagrams") do
     anagram = Anagram.new()
-    expect(anagram.anagram_antigram("ruby")).to(eq(true))
+    expect(anagram.anagram_antigram("ruby", "bury")).to(eq(true))
   end
 
   
