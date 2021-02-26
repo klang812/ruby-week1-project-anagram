5cc68e275164664e1d90fb08828f5d6770df3d06
Kerry Lang <klang812@gmail.com>
2021-02-26 10:34:36 -0800

    good fail test 1

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -2,10 +2,12 @@ class Anagram
 
 def anagram_antigram(word)
   word = word.split('').sort
-  new_word = word.include?.join
-    
-  end
+  #new_word = word.include?.join
+  # if word.match?(words) do
+  #   true
+  
+  # end
 end
-
+end
 
 words = ["ruby", "bury"]
