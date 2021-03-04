0480476ffb8cfd313de0eba2edbd6bad9e364fb2
Kerry Lang <klang812@gmail.com>
2021-03-04 10:20:43 -0800

    fix project errors

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,23 +1,78 @@
 class Anagram
-
-  def anagram_antigram(phrase)
+  
+  def two_words(phrase)
     word_list = phrase.split(' ')
     new_word_list = word_list.collect{|x| x.downcase.gsub(/\.|,|!|\s/,"").split('').sort.join}
     for word in new_word_list do
+      if new_word_list.count(word) > 1
+        return "These words are anagrams."
+      else
+        return "These words are not antigrams!"
+      end
+    end
+  end
+
+  def lower_case(phrase)
+    word_list = phrase.split(' ')
+    new_word_list = word_list.collect{|x| x.downcase.split('').sort.join}
+    for word in new_word_list do
+      if new_word_list.count(word) > 1
+        return "These words are anagrams."
+      else
+        return "These words are antigrams!"
+      end
+    end
+  end
+
+  def antigram(phrase)
+    word_list = phrase.split(' ')
+    new_word_list = word_list.collect{|x| x.downcase.split('').sort.join}
+    for word in new_word_list do
+      if new_word_list.count(word) > 1
+        return "These words are anagrams."
+      else
+        return "These words are antigrams!"
+      end
+    end
+  end
+
+  def vowels(phrase)
+    word_list = phrase.split(' ')
+    for word in word_list do
       letters = word.match(/[aeiou]/)         
       if letters.nil?
         return "These words have no vowels!"
-        end
+      end
+    end
+  end
+  
+  def no_punctuation(phrase)
+    word_list = phrase.split(' ')
+    new_word_list = word_list.collect{|x| x.downcase.gsub(/\.|,|!|\s/,"").split('').sort.join}
+    for word in new_word_list do
       if new_word_list.count(word) > 1
         return "These words are anagrams."
-      else return "These words have no letter matches and are antigrams!"
+      else
+        return "These words are antigrams!"
       end
-    end 
+    end
+  end
+
+  def sentences(phrase)
+    word_string = phrase.split(/\.|,|!|\?/)
+    word_list = word_string.collect{|x| x.gsub(/\s/,"")}
+    new_word_list = word_list.collect{|x| x.downcase.split('').sort.join}
+    for word in new_word_list do
+      if new_word_list.count(word) > 1
+        return "These words are anagrams."
+      else
+        return "These words are antigrams!"
+      end
+    end
   end
 end
+  
 
-words = ["ruby", "bury"]
-
-
+  
 
 
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -5,32 +5,31 @@ require('pry')
 describe('#anagram_antigram') do
   it("checks to see if 2 words are anagrams") do
     anagram = Anagram.new()
-    expect(anagram.anagram_antigram("ruby bury")).to(eq("These words are anagrams."))
+    expect(anagram.two_words("ruby bury")).to(eq("These words are anagrams."))
   end
 
   it("puts words into lower case to match") do
     anagram = Anagram.new()
-    expect(anagram.anagram_antigram("Ruby Bury")).to(eq("These words are anagrams."))
-  end
-
-  it("takes multi word string and finds anagram") do
-    anagram = Anagram.new()
-    expect(anagram.anagram_antigram("Ruby bury")).to(eq("These words are anagrams."))
+    expect(anagram.lower_case("Ruby Bury")).to(eq("These words are anagrams."))
   end
 
   it("take words with no matching letters") do
     anagram = Anagram.new()
-    expect(anagram.anagram_antigram("hi bye")).to(eq("These words have no letter matches and are antigrams!"))
+    expect(anagram.antigram("hi bye")).to(eq("These words are antigrams!"))
   end
 
   it("take words and check for vowels") do
     anagram = Anagram.new()
-    expect(anagram.anagram_antigram("thb bmkj")).to(eq("These words have no vowels!"))
+    expect(anagram.vowels("thb bmkj")).to(eq("These words have no vowels!"))
   end
   
   it("remove punctuation and spaces") do
     anagram = Anagram.new()
-    expect(anagram.anagram_antigram("ruby. bury")).to(eq("These words are anagrams."))
+    expect(anagram.no_punctuation("ruby. bury")).to(eq("These words are anagrams."))
   end
 
+  it("checks to see if 2 strings contain anagrams") do
+    anagram = Anagram.new()
+    expect(anagram.sentences("I love lucy. ilovelucy")).to(eq("These words are anagrams."))
+  end
 end
